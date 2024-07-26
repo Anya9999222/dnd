44397097a693210c2308e3b76d3784f9b15511bd
Anna <merkulova.anna96@gmail.com>
2024-07-26 15:44:28 +0300

    mistakes corrected

--- src/js/app.js
+++ src/js/app.js
@@ -11,130 +11,171 @@ thirdCol.bindToDOM();
 
 const cards = document.querySelectorAll(".cards");
 
+let clone;
 let actualElement;
-const leaveSpace = (el, height) => {
-  const div = document.createElement("div");
-  div.classList.add("additional");
-  div.style.height = height + "px";
-  el.appendChild(div);
-};
+let savedSpace;
 
-const removeAdditional = () => {
-  const additional = document.querySelector(".additional");
-  additional.remove();
-};
-const addSpace = (el, height) => {
-  const parent = el.closest(".cards");
-  const div = document.createElement("div");
-  div.classList.add("additional");
-  div.style.height = height + "px";
-  parent.insertBefore(el, div);
+const removeEl = (el) => {
+	el.remove();
 };
 
 cards.forEach((e) =>
-  e.addEventListener("mousedown", (e) => {
-    e.preventDefault();
-    leaveSpace(e.target.closest(".cards"), e.target.offsetHeight);
+	e.addEventListener("mousedown", (e) => {
+		e.preventDefault();
+		if (e.target.classList.contains("delete")) {
+			removeEl(e.target.closest(".card"));
+			return;
+		}
+		savedSpace = e.target.cloneNode(true);
+		savedSpace.classList.remove("card");
+		savedSpace.classList.add("saved");
+		savedSpace.style.height = e.target.offsetHeight + "px";
+		savedSpace.style.width = e.target.offsetWidth + "px";
+		savedSpace.querySelector(".delete").remove();
+		savedSpace.textContent = "";
 
-    actualElement = e.target;
-    document.body.style.cursor = "grabbing";
-    let shiftX = e.clientX - actualElement.getBoundingClientRect().left;
-    let shiftY = e.clientY - actualElement.getBoundingClientRect().top;
+		e.target.insertAdjacentElement("afterend", savedSpace);
 
-    document.body.append(actualElement);
-    actualElement.classList.add("dragged");
+		clone = e.target.cloneNode(true);
+		clone.classList.remove("card");
+		clone.classList.add("additional");
+		clone.style.height = e.target.offsetHeight + "px";
+		clone.style.width = e.target.offsetWidth + "px";
+		clone.querySelector(".delete").remove();
+		clone.textContent = "";
 
-    moveAt(e.pageX, e.pageY);
+		actualElement = e.target;
+		document.body.style.cursor = "grabbing";
+		let shiftX = e.clientX - actualElement.getBoundingClientRect().left;
+		let shiftY = e.clientY - actualElement.getBoundingClientRect().top;
 
-    const onMouseUp = (e) => {
-      const mouseUpItem = e.target;
-      const column = e.target.closest(".column");
-      const cardsBlock = column.querySelector(".cards");
-      document.body.style.cursor = "default";
-      cardsBlock.appendChild(actualElement);
-      actualElement.classList.remove("dragged");
-      actualElement = undefined;
-      removeAdditional();
+		actualElement.classList.add("dragged");
 
-      document.documentElement.removeEventListener("mouseup", onMouseUp);
-      document.documentElement.removeEventListener("mousemove", onMouseMove);
-      document.documentElement.removeEventListener("mouseover", onMouseOver);
-    };
+		document.body.append(actualElement);
 
-    function moveAt(pageX, pageY) {
-      actualElement.style.left = pageX - shiftX + "px";
-      actualElement.style.top = pageY - shiftY + "px";
-    }
-    const onMouseMove = (e) => {
-      moveAt(e.pageX, e.pageY);
-    };
+		moveAt(e.pageX, e.pageY);
 
-    const onMouseOver = (e) => {
-      // const cardOnTarget = e.target.classList.contains('card');
-      // if(cardOnTarget){
-      //     addSpace(e.target, e.target.style.height)
-      // }
-    };
+		const onMouseUp = (e) => {
+			const clonePos = document.querySelector(".additional");
+			const savedPos = document.querySelector(".saved");
+			if (clonePos) {
+				clonePos.insertAdjacentElement("beforebegin", actualElement);
+				removeEl(clonePos);
+			} else {
+				savedPos.insertAdjacentElement("beforebegin", actualElement);
+			}
+			removeEl(savedPos);
 
-    document.documentElement.addEventListener("mouseup", onMouseUp);
-    document.documentElement.addEventListener("mousemove", onMouseMove);
-    document.documentElement.addEventListener("mouseover", onMouseOver);
-  }),
+			document.body.style.cursor = "default";
+			actualElement.classList.remove("dragged");
+
+			actualElement = undefined;
+			clone = undefined;
+
+			document.documentElement.removeEventListener("mouseup", onMouseUp);
+			document.documentElement.removeEventListener("mousemove", onMouseMove);
+		};
+
+		function moveAt(pageX, pageY) {
+			actualElement.style.left = pageX - shiftX + "px";
+			actualElement.style.top = pageY - shiftY + "px";
+		}
+		const onMouseMove = (e) => {
+			const target = e.target;
+			const {
+				top
+			} = target.getBoundingClientRect();
+			const center = top + target.offsetHeight / 2;
+			const column = e.target.closest(".column");
+			if (!column) {
+				return;
+			}
+			const fantomOnBoard = document.querySelector(".additional");
+			const cardsBlock = column.querySelector(".cards");
+
+			if (target.classList.contains("card")) {
+				if (fantomOnBoard && !target.classList.contains("additional")) {
+					removeEl(fantomOnBoard);
+				}
+
+				if (e.clientY < center) {
+					target.insertAdjacentElement("beforebegin", clone);
+				} else {
+					target.insertAdjacentElement("afterend", clone);
+				}
+			} else if (target.classList.contains("cards")) {
+				const list = target.querySelectorAll(".card");
+				const pos = [];
+				list.forEach((item) => {
+					pos.push(item.getBoundingClientRect().top);
+				});
+
+				const index = pos.findIndex((i) => i > e.clientY);
+				cardsBlock.insertBefore(clone, list[index]);
+			} else {
+				cardsBlock.appendChild(clone);
+			}
+			moveAt(e.clientX, e.clientY);
+		};
+
+		document.documentElement.addEventListener("mouseup", onMouseUp);
+		document.documentElement.addEventListener("mousemove", onMouseMove);
+	}),
 );
 
 window.addEventListener("beforeunload", () => {
-  const data = {
-    firstCol: [],
-    secondCol: [],
-    thirdCol: [],
-  };
+	const data = {
+		firstCol: [],
+		secondCol: [],
+		thirdCol: [],
+	};
 
-  cards.forEach((e) => {
-    if (e.hasChildNodes()) {
-      const cards = e.querySelectorAll(".card");
-      cards.forEach((card) => {
-        if (card.closest(".column").classList.contains("first")) {
-          data.firstCol.push(card.innerText);
-        } else if (card.closest(".column").classList.contains("second")) {
-          data.secondCol.push(card.innerText);
-        } else {
-          data.thirdCol.push(card.innerText);
-        }
-      });
-    }
-  });
+	cards.forEach((e) => {
+		if (e.hasChildNodes()) {
+			const cards = e.querySelectorAll(".card");
+			cards.forEach((card) => {
+				if (card.closest(".column").classList.contains("first")) {
+					data.firstCol.push(card.innerText);
+				} else if (card.closest(".column").classList.contains("second")) {
+					data.secondCol.push(card.innerText);
+				} else {
+					data.thirdCol.push(card.innerText);
+				}
+			});
+		}
+	});
 
-  localStorage.setItem("data", JSON.stringify(data));
+	localStorage.setItem("data", JSON.stringify(data));
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  const json = localStorage.getItem("data");
+	const json = localStorage.getItem("data");
 
-  const firCol = document.querySelector(".first");
-  const secCol = document.querySelector(".second");
-  const thirCol = document.querySelector(".third");
+	const firCol = document.querySelector(".first");
+	const secCol = document.querySelector(".second");
+	const thirCol = document.querySelector(".third");
 
-  const cardBlocks = document.querySelectorAll(".cards");
-  let data;
+	const cardBlocks = document.querySelectorAll(".cards");
+	let data;
 
-  try {
-    data = JSON.parse(json);
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		data = JSON.parse(json);
+	} catch (error) {
+		console.log(error);
+	}
 
-  if (data) {
-    data.firstCol.forEach((item) => {
-      const card = new Card(firCol, item);
-      card.bindToDOM();
-    });
-    data.secondCol.forEach((item) => {
-      const card = new Card(secCol, item);
-      card.bindToDOM();
-    });
-    data.thirdCol.forEach((item) => {
-      const card = new Card(thirCol, item);
-      card.bindToDOM();
-    });
-  }
+	if (data) {
+		data.firstCol.forEach((item) => {
+			const card = new Card(firCol, item);
+			card.bindToDOM();
+		});
+		data.secondCol.forEach((item) => {
+			const card = new Card(secCol, item);
+			card.bindToDOM();
+		});
+		data.thirdCol.forEach((item) => {
+			const card = new Card(thirCol, item);
+			card.bindToDOM();
+		});
+	}
 });
